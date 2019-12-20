a7340afe3dad40bab5db9635f1248aaad937f4c5
Polina Drykova <polina.drykova1988@gmail.com>
2019-12-20 13:24:44 +0700

    finished functionality of orders

--- db/migrate/20191219041436_add_product_to_product_variants.rb
+++ db/migrate/20191219041436_add_product_to_product_variants.rb
@@ -1,5 +1,4 @@
 class AddProductToProductVariants < ActiveRecord::Migration[5.2]
   def change
-    add_reference :product_variants, :product, foreign_key: true, null: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_19_041737) do
+ActiveRecord::Schema.define(version: 2019_12_20_060041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,23 +19,28 @@ ActiveRecord::Schema.define(version: 2019_12_19_041737) do
     t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["title"], name: "index_categories_on_title", unique: true
   end
 
   create_table "order_items", force: :cascade do |t|
+    t.bigint "order_id", null: false
+    t.bigint "product_id", null: false
     t.integer "quantity", null: false
     t.decimal "price", precision: 15, scale: 2, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "order_id", null: false
     t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["product_id"], name: "index_order_items_on_product_id"
   end
 
   create_table "orders", force: :cascade do |t|
     t.string "first_name"
-    t.string "last_name", null: false
+    t.string "last_name"
     t.decimal "sub_total", precision: 15, scale: 2, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "token"
+    t.string "status", default: "cart"
   end
 
   create_table "product_categories", force: :cascade do |t|
@@ -48,24 +53,26 @@ ActiveRecord::Schema.define(version: 2019_12_19_041737) do
   end
 
   create_table "product_variants", force: :cascade do |t|
-    t.string "title", null: false
+    t.bigint "product_id", null: false
+    t.string "title", limit: 100, null: false
     t.decimal "price", precision: 15, scale: 2, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "product_id", null: false
     t.index ["product_id"], name: "index_product_variants_on_product_id"
   end
 
   create_table "products", force: :cascade do |t|
-    t.string "title", null: false
+    t.string "title", limit: 150, null: false
+    t.decimal "price", precision: 15, scale: 2, default: "0.0", null: false
     t.text "description"
-    t.decimal "price", precision: 15, scale: 2, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["title"], name: "index_products_on_title", unique: true
   end
 
-  add_foreign_key "order_items", "orders"
-  add_foreign_key "product_categories", "categories"
-  add_foreign_key "product_categories", "products"
-  add_foreign_key "product_variants", "products"
+  add_foreign_key "order_items", "orders", name: "fk_order_items_to_order"
+  add_foreign_key "order_items", "products", name: "fk_order_items_to_product"
+  add_foreign_key "product_categories", "categories", name: "fk_product_categories_to_categories"
+  add_foreign_key "product_categories", "products", name: "fk_product_categories_to_products"
+  add_foreign_key "product_variants", "products", name: "fk_product_variants_to_product"
 end
